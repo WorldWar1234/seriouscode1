28e1587697335416b51b52a15a38dedebbe87de4
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-30 15:53:20 +0530

    Update bypass.js

--- src/bypass.js
+++ src/bypass.js
@@ -1,13 +1,13 @@
 // Export the bypass function
-function bypass(req, res, buffer) {
+function bypass(req, res, output) {
   // Set the x-proxy-bypass header to indicate that the response was not modified
   res.setHeader('x-proxy-bypass', 1);
 
   // Set the content-length header to the length of the response buffer
-  res.setHeader('content-length', buffer.length);
+  res.setHeader('content-length', output.length);
 
   // Return a 200 OK response with the original buffer
-  res.status(200).send(buffer);
+  res.status(200).send(output);
 }
 
 // Export the bypass function
