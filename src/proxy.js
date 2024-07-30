7c2b0a22bbe662e0690e1c7b6edc58671016d730
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-30 14:26:08 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,4 +1,5 @@
 const sharp = require('sharp');
+const shouldCompress = require('./shouldCompress');
 
 function proxy(req, res) {
   sharp(req.params.url)
@@ -12,10 +13,12 @@ function proxy(req, res) {
       if (err) {
         console.error(err);
         res.status(500).send('Error processing image');
-      } else {
+      } else if (shouldCompress(req)) {
         res.setHeader('content-type', `image/${req.params.webp ? 'webp' : 'jpeg'}`);
         res.setHeader('content-length', info.size);
         res.status(200).send(output);
+      } else {
+        bypass(req, res, output);
       }
     });
 }
